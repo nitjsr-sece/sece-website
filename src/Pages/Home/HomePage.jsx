687e6cef93208e1d8b3d000beb61ca385cd0bcad
Abhishek <jawanpuriaabhishek@gmail.com>
2024-09-03 17:40:49 +0530

    scroll down button

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./HomePage.css";
 import img from "../../assets/logo-moon.png";
 import { Image } from "primereact/image";
-
+import { Link, animateScroll as scroll } from "react-scroll";
+import { IoIosArrowRoundDown } from "react-icons/io";
 const HomePage = () => {
   return (
     <div className="home-page">
@@ -16,14 +17,23 @@ const HomePage = () => {
         <div className="logo">
           <Image src={img} alt="Society Logo" />
         </div>
+        <div className="scroll">
+ <a href="#About">
+<IoIosArrowRoundDown />
+</a>
+</div>
       </div>
 
       {/* Second Section */}
-      <div className="content-section">
+      <div className="content-section" id="About">
         <h2>About Us</h2>
         <p>
-          [Insert detailed information about the society, its events, mission,
-          etc.]
+        Welcome to the Society of Electronics and Communication Engineering, where innovation meets excellence in the realm of advancing technology.
+        Our vision is to create a space where fresh ideas can turn into reality through passion and determination.
+        We organize a variety of events, workshops, and webinars throughout the year to keep students aware about the latest industry trends in the field of  electronics and software. 
+        This enables students to explore both domains and gain a clear understanding of their career paths.
+        We provide  a platform where students can engage themselves in a range of educational and entertaining events,
+        creating a perfect balance between learning and enjoyment.
         </p>
       </div>
 

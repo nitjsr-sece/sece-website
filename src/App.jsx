7c1354b79977b60c3d105f3a45fd72033320e4f8
debashis <debashisbaral469@gmail.com>
2024-09-05 16:53:28 +0530

    About us section chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import HomePage from "./Pages/Home/HomePage"
-import './index.css'; 
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/Home/HomePage.jsx";
+import CalendarPage from "./Pages/Calendar/CalendarPage.jsx";
+import EventsPage from "./Pages/Events/EventsPage.jsx";
+import NexusPage from "./Pages/Nexus/NexusPage.jsx";
+import TeamPage from "./Pages/Team/TeamPage.jsx";
+
+import "./index.css";
 
 function App() {
-
   return (
-    <>
-      <HomePage />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/nexus" element={<NexusPage />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/team" element={<TeamPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Pages/Nexus/NexusPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NexusPage = () => {
+  return (
+    <div>NexusPage</div>
+  )
+}
+
+export default NexusPage

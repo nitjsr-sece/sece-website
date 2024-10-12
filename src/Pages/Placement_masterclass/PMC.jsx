a050be02706fe2521a0616c33398332aac6ceda5
Abhishek <jawanpuriaabhishek@gmail.com>
2024-10-12 21:59:25 +0530

    added 23 batch in placement masterclass

--- src/Pages/Placement_masterclass/PMC.jsx
+++ src/Pages/Placement_masterclass/PMC.jsx
@@ -5,30 +5,8 @@ import NavDrawer from "../../components/Navbar/NavDrawer.jsx";
 import MenuButton from "../../components/MenuButton/MenuButton";
 import Navbar2 from "../../components/Navbar2";
 import Footer from '../../components/Footer/Footer';
-import ronit from '../../assets/placement_masterclass/ronit.png';
-import mv from '../../assets/placement_masterclass/moneyview.png';
-import ronit_mv_pmc from '../../assets/placement_masterclass/ronit-mv-pmc.jpg';
-import mohit_ranjan from '../../assets/placement_masterclass/mohit-ranjan.png';
-import paltech from '../../assets/placement_masterclass/paltech.png';
-import mohit_ranjan_pmc from '../../assets/placement_masterclass/mohit-ranjan-pmc.jpg';
-import aditya_kumar from '../../assets/placement_masterclass/aditya-kumar.png';
-import adiya_kumar_pmc from '../../assets/placement_masterclass/aditya-kumar-pmc.jpg';
-import intuit from '../../assets/placement_masterclass/intuit.jpg';
-import yogesh_kumar from '../../assets/placement_masterclass/yogesh-kumar.png';
-import yogesh_kumar_pmc from '../../assets/placement_masterclass/yogesh-kumar-pmc.jpg';
-import arcesium from '../../assets/placement_masterclass/arcesium.jpg';
-import rohit_kumar from '../../assets/placement_masterclass/rohit-kumar.png';
-import rohit_kumar_pmc from '../../assets/placement_masterclass/rohit-kumar-pmc.jpg';
-import Lowes from '../../assets/placement_masterclass/Lowes.jpg';
-import ishika_kumari from '../../assets/placement_masterclass/ishika-kumari.png';
-import ishika_kumari_pmc from '../../assets/placement_masterclass/ishika-kumari-pmc.jpg';
-import invesco from '../../assets/placement_masterclass/Invesco.jpg';
-import akanksha_priya from '../../assets/placement_masterclass/akanksha-priya.png';
-import akanksha_priya_pmc from '../../assets/placement_masterclass/akanksha-priya-pmc.jpg';
-import vikash_kumar from '../../assets/placement_masterclass/vikash-kumar.png';
-import vikash_kumar_pmc from '../../assets/placement_masterclass/vikash-kumar-pmc.jpg';
-import deshaw from '../../assets/placement_masterclass/deshaw.jpg';
-
+import { PMCData } from './PMCData.js';
+import { SentimentDissatisfied } from '@mui/icons-material';
 const MasterclassCard = ({ name, image, logo, pmc, index }) => {
     const [isOpen, setIsOpen] = useState(false);
 
@@ -50,15 +28,19 @@ const MasterclassCard = ({ name, image, logo, pmc, index }) => {
                 animate={{ opacity: 1, x: 0 }}
                 transition={{ duration: 0.75, delay: index * 0.2 }}
             >
-                <div className="flex items-center">
+                <div className="flex items-center masterclass-wrapper">
                     <img src={image} alt={name} className="person-img" />
-                    <div>
+                    <div className="pmc-name">
                         <h3 className="text-lg font-semibold">{name}</h3>
-                        <button className="mt-2 px-4 py-2 text-white rounded-3xl shadow-md hover:scale-110 transition-all ease-in duration-300" onClick={openImage}>
+                       
+                    </div>
+                    <div className="read-more">
+                    <button className="mt-2 px-4 py-2 text-white rounded-3xl shadow-md hover:scale-110 transition-all ease-in duration-300" onClick={openImage}>
                             Read More
                         </button>
-                    </div>
+                        </div>
                 </div>
+
                 <img src={logo} alt="Logo" className="company-logo" />
             </motion.div>
 
@@ -84,77 +66,49 @@ const MasterclassCard = ({ name, image, logo, pmc, index }) => {
 };
 
 const Masterclass = () => {
-    const PMCData = [
-        {
-            name: "Ronit Ranjan",
-            image: ronit,
-            logo: mv,
-            pmc: ronit_mv_pmc
-        },
-        {
-            name: "Mohit Ranjan",
-            image: mohit_ranjan,
-            logo: paltech,
-            pmc: mohit_ranjan_pmc
-        },
-        {
-            name: "Adiya Kumar",
-            image: aditya_kumar,
-            logo: intuit,
-            pmc: adiya_kumar_pmc
-        },
-        {
-            name: "Yogesh Kumar",
-            image: yogesh_kumar,
-            logo: arcesium,
-            pmc: yogesh_kumar_pmc
-        },
-        {
-            name: "Rohit Kumar",
-            image: rohit_kumar,
-            logo: Lowes,
-            pmc: rohit_kumar_pmc
-        },
-        {
-            name: "Ishika Kumari",
-            image: ishika_kumari,
-            logo: invesco,
-            pmc: ishika_kumari_pmc
-        },
-        {
-            name: "Akanksha Priya",
-            image: akanksha_priya,
-            logo: Lowes,
-            pmc: akanksha_priya_pmc
-        },
-        {
-            name: "Vikash Kumar",
-            image: vikash_kumar,
-            logo: deshaw,
-            pmc: vikash_kumar_pmc
-        }
-    ];
+  
     const [drawerOpen, setDrawerOpen] = useState(false);
+    const [batch,setBatch]=useState(24);
+    const batches={
+        "Batch 2k20-2k24":24,
+        "Batch 2k19-2k23":23
+    };
+    const handleBatchChange=(e)=>{
+           setBatch(e.target.value);
+    }
     const toggleDrawer = (isOpen) => {
         setDrawerOpen(isOpen);
     };
-
+    
     return (
         <div className="masterclass-container">
             <Navbar2 />
             <NavDrawer open={drawerOpen} onClose={() => toggleDrawer(false)} />
              <MenuButton onClick={() => toggleDrawer(true)} className="hamburger-home"/>
+
             <motion.h1 className="pmc_heading"
             initial={{ opacity: 0, y: -200 }}
-            animate={{ opacity: 1, y: 0 }}
+            animate={{ opacity: 1, y: 30 }}
             transition={{ duration: 0.75 }}
             >Placement Masterclass</motion.h1>
             <motion.p className="text-center mb-12 by-sece"
             initial={{ opacity: 0, y: -200 }}
-            animate={{ opacity: 1, y: 0 }}
+            animate={{ opacity: 1, y: 25 }}
             transition={{ duration: 0.5 }}
             >By SECE</motion.p>
-            {PMCData.map((data, i) => (
+        <select
+              className="px-6 py-3 rounded-full border border-gray-300 text-gray-700 text-xl bg-white transition-all duration-300 shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-sky-500 pmc-select"
+              value={batch}
+              onChange={handleBatchChange}
+            >
+              {Object.keys(batches).map((bt) => (
+                <option key={batches[bt]} value={batches[bt]}>
+                 {bt}
+                </option>
+              ))}
+            </select>
+            {PMCData.filter((dt)=>{return dt.batch==batch}).map((data, i) => (
+                
                 <MasterclassCard
                     key={i}
                     index={i}
---
+++ src/Pages/Placement_masterclass/PMCData.js
@@ -0,0 +1,184 @@
+//batch24
+import ronit from '../../assets/placement_masterclass/batch_24/ronit.png';
+import mv from '../../assets/placement_masterclass/company_logo/moneyview.png';
+import ronit_mv_pmc from '../../assets/placement_masterclass/batch_24/ronit-mv-pmc.jpg';
+import mohit_ranjan from '../../assets/placement_masterclass/batch_24/mohit-ranjan.png';
+import paltech from '../../assets/placement_masterclass/company_logo/paltech.png';
+import mohit_ranjan_pmc from '../../assets/placement_masterclass/batch_24/mohit-ranjan-pmc.jpg';
+import aditya_kumar from '../../assets/placement_masterclass/batch_24/aditya-kumar.png';
+import adiya_kumar_pmc from '../../assets/placement_masterclass/batch_24/aditya-kumar-pmc.jpg';
+import intuit from '../../assets/placement_masterclass/company_logo/intuit.jpg';
+import yogesh_kumar from '../../assets/placement_masterclass/batch_24/yogesh-kumar.png';
+import yogesh_kumar_pmc from '../../assets/placement_masterclass/batch_24/yogesh-kumar-pmc.jpg';
+import arcesium from '../../assets/placement_masterclass/company_logo/arcesium.jpg';
+import rohit_kumar from '../../assets/placement_masterclass/batch_24/rohit-kumar.png';
+import rohit_kumar_pmc from '../../assets/placement_masterclass/batch_24/rohit-kumar-pmc.jpg';
+import Lowes from '../../assets/placement_masterclass/company_logo/Lowes.jpg';
+import ishika_kumari from '../../assets/placement_masterclass/batch_24/ishika-kumari.png';
+import ishika_kumari_pmc from '../../assets/placement_masterclass/batch_24/ishika-kumari-pmc.jpg';
+import invesco from '../../assets/placement_masterclass/company_logo/Invesco.jpg';
+import akanksha_priya from '../../assets/placement_masterclass/batch_24/akanksha-priya.png';
+import akanksha_priya_pmc from '../../assets/placement_masterclass/batch_24/akanksha-priya-pmc.jpg';
+import vikash_kumar from '../../assets/placement_masterclass/batch_24/vikash-kumar.png';
+import vikash_kumar_pmc from '../../assets/placement_masterclass/batch_24/vikash-kumar-pmc.jpg';
+import deshaw from '../../assets/placement_masterclass/company_logo/deshaw.jpg';
+
+//batch23
+import aditya_raj_srivastava from '../../assets/placement_masterclass/batch_23/aditya-raj-srivastava.jpg';
+import aditya_raj_srivastava_pmc from '../../assets/placement_masterclass/batch_23/aditya-raj-srivastava-pmc.jpg';
+import arpit_raj from '../../assets/placement_masterclass/batch_23/arpit-raj.jpg';
+import arpit_raj_pmc from '../../assets/placement_masterclass/batch_23/arpit-raj-pmc.jpg';
+import ayush_kr_chaudhary from '../../assets/placement_masterclass/batch_23/ayush-kr-chaudhary.jpg';
+import ayush_kr_chaudhary_pmc from '../../assets/placement_masterclass/batch_23/ayush-kr-chaudhary-pmc.jpg';
+import mohit_mayank from '../../assets/placement_masterclass/batch_23/mohit-mayank.jpg';
+import mohit_mayank_pmc from '../../assets/placement_masterclass/batch_23/mohit-mayank-pmc.jpg';
+import naved_sohail from '../../assets/placement_masterclass/batch_23/naved-sohail.jpg';
+import naved_sohail_pmc from '../../assets/placement_masterclass/batch_23/naved-sohail-pmc.jpg';
+import priya_singh from '../../assets/placement_masterclass/batch_23/priya-singh.jpg';
+import priya_singh_pmc from '../../assets/placement_masterclass/batch_23/priya-singh-pmc.jpg';
+import raman_kumar from '../../assets/placement_masterclass/batch_23/raman-kumar.jpg';
+import raman_kumar_pmc from '../../assets/placement_masterclass/batch_23/raman-kumar-pmc.jpg';
+import ritika_kumari from '../../assets/placement_masterclass/batch_23/ritika-kumari.jpg';
+import ritika_kumar_pmc from '../../assets/placement_masterclass/batch_23/ritika-kumari-pmc.jpg';
+import shashank_shekhar from '../../assets/placement_masterclass/batch_23/shashank-shekhar.jpg';
+import shashank_shekhar_pmc from '../../assets/placement_masterclass/batch_23/shashank-shekhar-pmc.jpg';
+import yash_baranwal from '../../assets/placement_masterclass/batch_23/yash-baranwal.jpg';
+import yash_baranwal_pmc from '../../assets/placement_masterclass/batch_23/yash-baranwal-pmc.jpg';
+
+import amazon from '../../assets/placement_masterclass/company_logo/amazon.jpg';
+import atlassian from '../../assets/placement_masterclass/company_logo/atlassian.jpg';
+import olx_autos from '../../assets/placement_masterclass/company_logo/olx-autos.jpg';
+import oracle from '../../assets/placement_masterclass/company_logo/oracle.jpg';
+import Qualcomm from '../../assets/placement_masterclass/company_logo/Qualcomm.png';
+import vyapar from '../../assets/placement_masterclass/company_logo/vyapar.jpg';
+import yellowai from '../../assets/placement_masterclass/company_logo/yellowai.jpg';
+
+
+export const PMCData = [
+    {
+        name: "Ronit Ranjan",
+        image: ronit,
+        logo: mv,
+        pmc: ronit_mv_pmc,
+        batch:24
+    },
+    {
+        name: "Mohit Ranjan",
+        image: mohit_ranjan,
+        logo: paltech,
+        pmc: mohit_ranjan_pmc,
+        batch:24
+    },
+    {
+        name: "Adiya Kumar",
+        image: aditya_kumar,
+        logo: intuit,
+        pmc: adiya_kumar_pmc,
+        batch:24
+    },
+    {
+        name: "Yogesh Kumar",
+        image: yogesh_kumar,
+        logo: arcesium,
+        pmc: yogesh_kumar_pmc,
+        batch:24
+    },
+    {
+        name: "Rohit Kumar",
+        image: rohit_kumar,
+        logo: Lowes,
+        pmc: rohit_kumar_pmc,
+        batch:24
+    },
+    {
+        name: "Ishika Kumari",
+        image: ishika_kumari,
+        logo: invesco,
+        pmc: ishika_kumari_pmc,
+        batch:24
+    },
+    {
+        name: "Akanksha Priya",
+        image: akanksha_priya,
+        logo: Lowes,
+        pmc: akanksha_priya_pmc,
+        batch:24
+    },
+    {
+        name: "Vikash Kumar",
+        image: vikash_kumar,
+        logo: deshaw,
+        pmc: vikash_kumar_pmc,
+        batch:24
+    },
+    {
+        name:"Raman Kumar",
+        image:raman_kumar,
+        logo:vyapar,
+        pmc:raman_kumar_pmc,
+        batch:23
+    },
+    {
+        name:"Shashank Shekhar",
+        image:shashank_shekhar,
+        logo:Qualcomm,
+        pmc:shashank_shekhar_pmc,
+        batch:23
+    },
+    {
+        name:"Ritika Kumari",
+        image:ritika_kumari,
+        logo:olx_autos,
+        pmc:ritika_kumar_pmc,
+        batch:23
+    },
+    {
+        name:"Ayush Kumar Chaudhary",
+        image:ayush_kr_chaudhary,
+        logo:yellowai,
+        pmc:ayush_kr_chaudhary_pmc,
+        batch:23
+    },
+    {
+        name:"Aditya Raj Srivastava",
+        image:aditya_raj_srivastava,
+        logo:amazon,
+        pmc:aditya_raj_srivastava_pmc,
+        batch:23
+    },
+    {
+        name:"Priya Singh",
+        image:priya_singh,
+        logo:Lowes,
+        pmc:priya_singh_pmc,
+        batch:23
+    },
+    {
+        name:"Naved Sohail",
+        image:naved_sohail,
+        logo:amazon,
+        pmc:naved_sohail_pmc,
+        batch:23
+    },
+    {
+        name:"Aript Raj",
+        image:arpit_raj,
+        logo:oracle,
+        pmc:arpit_raj_pmc,
+        batch:23
+    },
+    {
+        name:"Yash Baranwal",
+        image:yash_baranwal,
+        logo:atlassian,
+        pmc:yash_baranwal_pmc,
+        batch:23
+    },
+    {
+        name:"Mohit Mayank",
+        image:mohit_mayank,
+        logo:Qualcomm,
+        pmc:mohit_mayank_pmc,
+        batch:23
+    }
+];

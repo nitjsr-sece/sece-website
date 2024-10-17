f9955bff37a7daf48fadbd378bf84f7aed063569
Abhishek <jawanpuriaabhishek@gmail.com>
2024-10-18 00:14:58 +0530

    readded nexus logo

--- src/Pages/Nexus/CountdownTimer.jsx
+++ src/Pages/Nexus/CountdownTimer.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './CountdownTimer.css';
-import nexus_logo from '../../assets/nexus-funky-logo.png';
+
 const CountdownTimer = () => {
   const calculateTimeLeft = () => {
     const targetDate = new Date('October 18, 2024 00:00:00 GMT+0530'); // 18th Oct 12:00 AM IST
@@ -36,7 +36,7 @@ const CountdownTimer = () => {
    ( <>
     
      
-    <img src={nexus_logo} className="nexus-funky-logo"></img>
+    
       <div className="time-display drop-shadow-[0_4px_10px_#9375c6]">
         {timeLeft.hours !== undefined
           ? `${timeLeft.hours.toString().padStart(2, '0')}:${timeLeft.minutes.toString().padStart(2, '0')}:${timeLeft.seconds.toString().padStart(2, '0')}`
